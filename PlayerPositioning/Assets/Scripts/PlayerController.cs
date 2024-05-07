ec51a06c2e88f7248a4b501c29e97ae886751c8e
Charles Kranich <kranichc@roverkids.org>
2024-05-07 14:02:14 -0400

    2.2 step 2

--- PlayerPositioning/Assets/Scripts/PlayerController.cs
+++ PlayerPositioning/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float leftBoundary = -10.0f;
     public float rightBoundary = 10.0f;
 
+    public GameObject projectPrefile;
+
     // Start is called before the first frame update
     void Start()
     {
