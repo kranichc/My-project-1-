35d7661fae0c82fe911c15ba91486ad8f74537e2
Charles Kranich <kranichc@roverkids.org>
2024-05-13 13:18:33 -0400

    2.4 step 1

--- PlayerPositioning/Assets/Scripts/SpawnManager.cs
+++ PlayerPositioning/Assets/Scripts/SpawnManager.cs
@@ -18,10 +18,16 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.S))
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
